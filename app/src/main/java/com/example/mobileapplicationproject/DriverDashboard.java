e45b9f1d8a3e6f7c8f45f99a92e07bebbc98a4b2
Shyneewalzer <shyneewalzer@gmail.com>
2021-01-18 16:36:57 +0800

    :sparkel: new

--- app/src/main/java/com/example/mobileapplicationproject/DriverDashboard.java
+++ app/src/main/java/com/example/mobileapplicationproject/DriverDashboard.java
@@ -134,7 +134,7 @@ public class DriverDashboard extends AppCompatActivity implements NavigationView
             }
         });
 
-    //TODO: Set up Employee Dashboard for QR
+    //TODO: Set up Employee Dashboard for QR......
     }
 
     private class Dbread extends AsyncTask<String, String, String>
