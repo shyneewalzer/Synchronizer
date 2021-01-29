9bc332afd2ed035579cde8fec600bb61a1b83693
Shyneewalzer <shyneewalzer@gmail.com>
2021-01-29 10:17:53 +0800

    :sparkel: new

--- app/src/main/java/com/example/mobileapplicationproject/FragmentEstabDetailsHistory.java
+++ app/src/main/java/com/example/mobileapplicationproject/FragmentEstabDetailsHistory.java
@@ -501,6 +501,7 @@ public class FragmentEstabDetailsHistory extends Fragment implements View.OnClic
                                     parentid.add(rsid.getString("account_id"));
                                     timee.add(rsid.getString("time_entered"));
                                     datee.add(rsid.getString("date_entered"));
+                                    empid.add(rsid.getString("employee_id"));
 
                                     listGroup.add(rsid.getString("batch"));
 
